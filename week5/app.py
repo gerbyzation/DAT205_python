9c26caab06000bb1ae9d69b5845e01d921e6d1d7
Gerben Neven <gerbyzation@me.com>
2015-03-20 17:06:49 +0000

    cast count from int to string

--- week5/app.py
+++ week5/app.py
@@ -34,6 +34,6 @@ console.close()
 
 api = twitter.Api(consumer_key=keys[0], consumer_secret=keys[1], access_token_key=keys[2], access_token_secret=keys[3])
 
-response = api.PostUpdate("I visited " + title + " " + nView + " times in #Chrome #pythonbot #wearecoming")
+response = api.PostUpdate("I visited " + title + " " + str(nView) + " times in #Chrome #pythonbot #wearecoming")
 
 print("Status posted:s " + response.text)
