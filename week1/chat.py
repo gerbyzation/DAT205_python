327e0f8122eb95fca9511f29ffec351bd73fb3e6
Gerben Neven <gerbyzation@me.com>
2015-03-27 17:05:55 +0000

    updating week 1 & 4

--- week1/chat.py
+++ week1/chat.py
@@ -1,13 +1,22 @@
+# read stopwords file
 file = open("stopwords.txt")
 stopwordsRaw = file.readlines()
 stopwords = []
 
+# read countries file
+countries = []
+countriesRaw = open("countries.txt").readlines()
+for country in countriesRaw:
+    countries.append(country.split('|')[1].strip())
+
+person = {}
+
 for word in stopwordsRaw:
     stopwords.append(word.strip())
 
-# removes stopwords from input
-def filter(input):
-    print stopwords
+
+def ask(question):
+    input = raw_input(question + " ")
     words = input.split(' ')
     for word in words:
         if word in stopwords:
@@ -15,7 +24,26 @@ def filter(input):
             words.remove(word)
     return ' '.join(words)
 
-while True:
-    input = raw_input("Say something interesting: ")
-    filtered = filter(input)
-    print('filtered: ' + filtered)
+##########
+# Asking for name
+##########
+nameInput = ask("Hi! What's your name?")
+# assuming people always mention their name last
+name = nameInput.split(' ')[-1]
+person['name'] = name
+print "Nice to meet you " + name + "!"
+
+locationInput = ask("In what country do you live?")
+locWords = locationInput.split(' ')
+for word in locWords:
+    print locWords
+    if locWords in countries:
+        print "I have always wanted to go to " + word + "! Have a few friends who live there."
+    else:
+        print "Cool!"
+
+input = raw_input("Say something interesting: ")
+filtered = filter(input)
+print('filtered: ' + filtered)
+
+# List of countries foudn at: https://gist.github.com/marijn/396531
--- week4/app.py
+++ week4/app.py
@@ -1,10 +1,30 @@
 import twitter
 import datetime
-
-user = 214990133
+import re
+import urllib2
+from bs4 import BeautifulSoup
 
 file = open('auth.txt')
 keys = file.readline().strip().split(',')
+
+historyFile = open("/Users/gerbenneven/Library/Application Support/Google/Chrome/Default/Current Session")
+history = historyFile.read()
+historyFile.close()
+
+start = history.rfind('http')
+end = history.find(chr(0), start)
+url = history[start:end]
+
+page = urllib2.urlopen(url)
+body = page.read()
+
+soup = BeautifulSoup(body)
+title = soup.html.head.title.contents
+print(url)
+print(title)
+
+# urls = re.findall("http[s]?://[a-zA-Z]|[0-9]|[.]/", history)
+
 
 api = twitter.Api(consumer_key=keys[0], consumer_secret=keys[1], access_token_key=keys[2], access_token_secret=keys[3])
 
